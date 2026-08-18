ae3168b82cb3fccc9bc14df447d4dddd1f47df8b
Aditansh Kashyap <aditanshkashyap@gmail.com>
2026-08-18 19:04:41 +0530

    Feature: add Scandinavian and Eastern European languages

--- frontend/src/lib/languages.ts
+++ frontend/src/lib/languages.ts
@@ -8,6 +8,15 @@ export const LANGUAGES = [
   { id: 'dutch', label: 'Dutch', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/dutch.json' },
   { id: 'swedish', label: 'Swedish', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/swedish.json' },
   { id: 'danish', label: 'Danish', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/danish.json' },
+  { id: 'norwegian_bokmal', label: 'Norwegian Bokmål', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/norwegian_bokmal.json' },
+  { id: 'norwegian_nynorsk', label: 'Norwegian Nynorsk', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/norwegian_nynorsk.json' },
+  { id: 'finnish', label: 'Finnish', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/finnish.json' },
+  { id: 'icelandic', label: 'Icelandic', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/icelandic.json' },
+  { id: 'irish', label: 'Irish', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/irish.json' },
+  { id: 'welsh', label: 'Welsh', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/welsh.json' },
+  { id: 'polish', label: 'Polish', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/polish.json' },
+  { id: 'czech', label: 'Czech', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/czech.json' },
+  { id: 'slovak', label: 'Slovak', url: 'https://raw.githubusercontent.com/monkeytypegame/monkeytype/master/frontend/static/languages/slovak.json' },
 ];
 
 export function getLanguageUrl(id: string): string {
